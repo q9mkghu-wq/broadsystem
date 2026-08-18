1a5b767dd89a0592a1b095c73b27b67686e2c618
q9mkghu-wq <q9mkghu@gmail.com>
2026-08-18 11:07:17 +0900

    Update Firebase config to use environment variables

--- src/firebase.js
+++ src/firebase.js
@@ -1,14 +1,16 @@
 import { initializeApp } from "firebase/app";
 import { getFirestore } from "firebase/firestore";
+import { getAuth } from "firebase/auth";
 
 const firebaseConfig = {
-apiKey: "AIzaSyBvwf-pR1WYdFOnJGuhbrnf02HTNhVgSaA",
-  authDomain: "broadsystem-9147f.firebaseapp.com",
-  projectId: "broadsystem-9147f",
-  storageBucket: "broadsystem-9147f.firebasestorage.app",
-  messagingSenderId: "458957971815",
-  appId: "1:458957971815:web:0e71b6ccb7a4d0936b4fc2"
+  apiKey: import.meta.env.VITE_FIREBASE_API_KEY,
+  authDomain: import.meta.env.VITE_FIREBASE_AUTH_DOMAIN,
+  projectId: import.meta.env.VITE_FIREBASE_PROJECT_ID,
+  storageBucket: import.meta.env.VITE_FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: import.meta.env.VITE_FIREBASE_MESSAGING_SENDER_ID,
+  appId: import.meta.env.VITE_FIREBASE_APP_ID,
 };
 
 export const app = initializeApp(firebaseConfig);
 export const db = getFirestore(app);
+export const auth = getAuth(app);
